8752142c29ab415f8f254d06fdc1cb2c72bc6634
Andrew <agordonw@comcast.net>
2015-07-19 20:33:45 -0400

    added blur, changed for clarity

--- myCartoonify.py
+++ myCartoonify.py
@@ -1,28 +1,45 @@
 import cv2
 import numpy as np
 import scipy as sp
+import scipy.signal
 
-def cartoonify(image, howChunky):
-	height, width, channels = image.shape
+def cartoonify(image, howChunky, blurNum):
+    height, width, channels = image.shape
+    increment = 256/howChunky
+    count = 0.0
+    total = height * width
+    image = cv2.blur(image, (blurNum, blurNum))
 
-	for i in range(height):
-		for j in range(width):
-			pixel = image[i, j]
-			r, g, b = pixel
+    print image.shape
 
-			val = 266/howChunky
+    for i in range(height):
+        for j in range(width):
 
-			for x in range(val - 1):
-				if (x*val <= r & r <= (x+1)*val):
-					r = ((x*val) + ((x+1)*val)) / 2
-				elif (x*val <= g & g <= (x+1)*val):
-					g = ((x*val) + ((x+1)*val)) / 2
-				elif (x*val <= b & b <= (x+1)*val):
-					b = ((x*val) + ((x+1)*val)) / 2
+            print count/total, " percent done"
+            count += 1
 
-			image[i, j] = r, g, b
+            pixel = image[i, j]
+            r, g, b = pixel         
 
-	cv2.imwrite("colorsOut.png", image)
+            for x in range(howChunky):
+                # print x/howChunky, " percent done"
+                floor = x * increment
+                ceil = (x+1) * increment
 
-testImage = cv2.imread("colors.png", cv2.IMREAD_COLOR)
-cartoonify(testImage, 2)
+                if (r >= floor and r < ceil):
+                    r = ((floor) + (ceil)) / 2
+
+                elif (g >= floor and g < ceil):
+                    g = ((floor) + (ceil)) / 2
+
+                elif (b >= floor and b < ceil):
+                    b = ((floor) + (ceil)) / 2
+
+            image[i, j] = r, g, b
+
+
+    cv2.imwrite("colorsOut.png", image)
+
+
+testImage = cv2.imread("kite.png", cv2.IMREAD_COLOR)
+cartoonify(testImage, 4, 5)
